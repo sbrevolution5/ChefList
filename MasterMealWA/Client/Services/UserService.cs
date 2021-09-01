4822be9997dfb375c4322e424c3d5ece6f2c53ad
sbrevolution5 <sbrevolution5@aim.com>
2021-09-01 15:12:29 -0400

    add and implemnt user service

---
+++ MasterMealWA/Client/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using MasterMealWA.Client.Services.Interfaces;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterMealWA.Client.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AuthenticationStateProvider _authState;
+
+        public UserService(AuthenticationStateProvider authState)
+        {
+            _authState = authState;
+        }
+
+        public async Task<string> GetUserIdAsync()
+        {
+            var id = (await _authState.GetAuthenticationStateAsync()).User.FindFirst(c => c.Type == "sub")?.Value;
+            return id;
+        }
+    }
+}
