500b7dc31e71bfb4cb4a1efd1db4b099480fb27d
sbrevolution5 <sbrevolution5@aim.com>
2021-09-01 16:46:21 -0400

    migrations

---
+++ MasterMealWA/Server/Migrations/20210901204205_007-privaterecipe.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MasterMealWA.Server.Migrations
+{
+    public partial class _007privaterecipe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrivate",
+                table: "Recipe",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrivate",
+                table: "Recipe");
+        }
+    }
+}
