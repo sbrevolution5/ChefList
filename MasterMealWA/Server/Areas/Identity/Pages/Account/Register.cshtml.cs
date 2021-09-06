b27fe1eef5f0dcc728f55bb3cbd5a436fcac0512
sbrevolution5 <sbrevolution5@aim.com>
2021-09-06 08:45:02 -0400

    added fields to register

--- MasterMealWA/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MasterMealWA/Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -50,6 +50,15 @@ namespace MasterMealWA.Server.Areas.Identity.Pages.Account
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+            [Required]
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
@@ -75,7 +84,14 @@ namespace MasterMealWA.Server.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new Chef { UserName = Input.Email, Email = Input.Email };
+                var user = new Chef
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    ScreenName = Input.UserName
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
