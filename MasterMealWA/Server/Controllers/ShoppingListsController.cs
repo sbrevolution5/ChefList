40242617771628bd1968e2866985260747c0a097
sbrevolution5 <sbrevolution5@aim.com>
2021-09-06 09:42:57 -0400

    fix userid for shopping list

--- MasterMealWA/Server/Controllers/ShoppingListsController.cs
+++ MasterMealWA/Server/Controllers/ShoppingListsController.cs
@@ -46,7 +46,7 @@ namespace MasterMealWA.Server.Controllers
         public async Task<ActionResult<ShoppingList>> GetShoppingList(int id)
         {
             var shoppingList = await _context.ShoppingList.Include(l => l.ShoppingIngredients).ThenInclude(s => s.Ingredient).Where(l => l.Id == id).FirstOrDefaultAsync();
-            var userId = _userManager.GetUserId(User);
+            var userId = HttpContext.GetUserId();
 
             if (shoppingList == null || shoppingList.ChefId != userId)
             {
