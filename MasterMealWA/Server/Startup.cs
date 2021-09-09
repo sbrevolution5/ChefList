8ead260bb931bce2b7bf8618cdfee93c433bae9c
sbrevolution5 <sbrevolution5@aim.com>
2021-09-09 16:38:08 -0400

    back to hsts

--- MasterMealWA/Server/Startup.cs
+++ MasterMealWA/Server/Startup.cs
@@ -78,7 +78,7 @@ namespace MasterMealWA.Server
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseReverseProxyHttpsEnforcer();
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
