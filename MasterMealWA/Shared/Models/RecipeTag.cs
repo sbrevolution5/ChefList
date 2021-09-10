b39a8f4a01b45336e4c9e92b99993d4a6c447836
sbrevolution5 <sbrevolution5@aim.com>
2021-09-10 15:45:29 -0400

    add required attributes

--- MasterMealWA/Shared/Models/RecipeTag.cs
+++ MasterMealWA/Shared/Models/RecipeTag.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace MasterMealWA.Shared.Models
     public class RecipeTag
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
     }
