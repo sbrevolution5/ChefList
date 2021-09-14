43aedca74fbf93cfc33f5b2dae0b48965f4a3d40
sbrevolution5 <sbrevolution5@aim.com>
2021-09-14 09:41:38 -0400

    add filter model properties

--- MasterMealWA/Client/Models/Filter.cs
+++ MasterMealWA/Client/Models/Filter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MudBlazor;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,18 @@ namespace MasterMealWA.Client.Models
 {
     public class Filter
     {
+        public bool MyRecipes { get; set; }
+        public bool MySupplies { get; set; }
+        public MudChip[] NationalityChips { get; set; }
+        public bool HasAllNationality { get; set; }
+        public MudChip[] ProteinChips { get; set; }
+        public bool HasAllProtein { get; set; }
+        public MudChip[] TypeChips { get; set; }
+        public bool HasAllType { get; set; }
+        public int MaxRating { get; set; } = 5;
+        public int MinRating { get; set; } = 1;
+        public bool CookingTime { get; set; }
+        public int MinCookingTime { get; set; }
+        public int MaxCookingTime { get; set; }
     }
 }
