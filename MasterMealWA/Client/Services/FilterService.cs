a7d51709cd24c4ff6e04d4c1bf00fc3d644d376a
sbrevolution5 <sbrevolution5@aim.com>
2021-09-14 10:08:42 -0400

    Implement interface and create service

---
+++ MasterMealWA/Client/Services/FilterService.cs
@@ -0,0 +1,18 @@
+﻿using MasterMealWA.Client.Models;
+using MasterMealWA.Client.Services.Interfaces;
+using MasterMealWA.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterMealWA.Client.Services
+{
+    public class FilterService : IFilterService
+    {
+        public List<Recipe> ApplyFilter(List<Recipe> allRecipes, Filter filter)
+        {
+            //
+        }
+    }
+}
