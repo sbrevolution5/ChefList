7347619faa8356c44722e689cf7e92c2d6b71abe
sbrevolution5 <sbrevolution5@aim.com>
2021-09-19 12:20:29 -0400

    add new service for recipe conversion

---
+++ MasterMealWA/Server/Services/ServingService.cs
@@ -0,0 +1,25 @@
+﻿using MasterMealWA.Server.Data;
+using MasterMealWA.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterMealWA.Server.Services
+{
+    public class ServingService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServingService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Recipe> ScaleRecipeAsync(int recipeId, int desiredServings)
+        {
+            var singleServe = ConvertRecipeToSingleServing();
+            var correctServing = UpscaleServing()
+            throw new NotImplementedException();
+        }
+    }
+}
