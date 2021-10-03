5a8509387f13a726e43b41335003954016499363
sbrevolution5 <sbrevolution5@aim.com>
2021-10-03 12:09:03 -0400

    Add dto for shoppiung ingredient to add

---
+++ MasterMealWA/Shared/Models/Dtos/AddToShoppingDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMealWA.Shared.Models.Dtos
+{
+    class AddToShoppingDto
+    {
+        public ShoppingIngredient Ingredient { get; set; }
+        public int ListId { get; set; }
+    }
+}
