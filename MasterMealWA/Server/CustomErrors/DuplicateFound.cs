f2e8b9d94db2a2d37c927b1cd944be93085d7dd8
sbrevolution5 <sbrevolution5@aim.com>
2021-10-04 13:24:26 -0400

    add duplicate found result

---
+++ MasterMealWA/Server/CustomErrors/DuplicateFound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MasterMealWA.Server.CustomErrors
+{
+    public class DuplicateFound : IHttpActionResult
+    {
+        private string message;
+
+        public DuplicateFound(string message)
+        {
+            this.message = message;
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+            response.Content = new StringContent(message);
+            return Task.FromResult(response);
+        }
+    }
+}
