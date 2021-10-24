7e7419edabe8f07a72ae0374c5caa258a831cde9
sbrevolution5 <sbrevolution5@aim.com>
2021-10-24 14:31:34 -0400

    create report model

---
+++ MasterMealWA/Shared/Models/Social/Report.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMealWA.Shared.Models.Social
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public string Reason { get; set; }
+        public DateTime Submitted { get; set; }
+        public Guid SubmitterId { get; set; }
+        public virtual Chef Submitter { get; set; }
+        public virtual Recipe Recipe { get; set; }
+        public bool Reviewed { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
