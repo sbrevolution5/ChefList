801149f9e614a9fcd6b368225bab0ac64be38d0e
sbrevolution5 <sbrevolution5@aim.com>
2021-10-24 14:36:10 -0400

    new migrations and data context

---
+++ MasterMealWA/Server/Migrations/20211024183515_021-Report.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MasterMealWA.Server.Migrations
+{
+    public partial class _021Report : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RecipeId = table.Column<int>(type: "integer", nullable: false),
+                    Reason = table.Column<string>(type: "text", nullable: true),
+                    Submitted = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    SubmitterId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SubmitterId1 = table.Column<string>(type: "text", nullable: true),
+                    Reviewed = table.Column<bool>(type: "boolean", nullable: false),
+                    ReviewDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Report_AspNetUsers_SubmitterId1",
+                        column: x => x.SubmitterId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Report_Recipe_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipe",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_RecipeId",
+                table: "Report",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_SubmitterId1",
+                table: "Report",
+                column: "SubmitterId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
