30bd18da927145cab639e312932413cbe1d777d4
sbrevolution5 <sbrevolution5@aim.com>
2021-10-26 11:34:03 -0400

    finish implementing volume

--- MasterMealWA/Server/Services/MeasurementService.cs
+++ MasterMealWA/Server/Services/MeasurementService.cs
@@ -38,106 +38,66 @@ namespace MasterMealWA.Server.Services
         {
             VolumeMeasurementUnit unit;
             int conversionFactor;
-            string unitString;
             int fracTSP = ingredient.Quantity;
             if (fracTSP >= 4 * 2 * 2 * 8 * 2 * 3 * 24)
             {
-                unitString = "Gallon";
                 unit = VolumeMeasurementUnit.Gallon;
                 conversionFactor = 4 * 2 * 2 * 8 * 2 * 3 * 24;
             }
             else if (fracTSP >= 2 * 2 * 8 * 2 * 3 * 24)
             {
-                unitString = "Quart";
                 unit = VolumeMeasurementUnit.Quart;
                 conversionFactor = 2 * 2 * 8 * 2 * 3 * 24;
             }
             else if (fracTSP >= 2 * 8 * 2 * 3 * 24)
             {
-                unitString = "Pint";
                 unit = VolumeMeasurementUnit.Pint;
                 conversionFactor = 2 * 8 * 2 * 3 * 24;
             }
             else if (fracTSP >= 4 * 3 * 24)
             {
-                unitString = "Cup";
                 unit = VolumeMeasurementUnit.Cup;
                 conversionFactor = 8 * 2 * 3 * 24;
 
             }
-            //else if (fracTSP >= 24 * 3 * 2)
-            //{
-            //    unitString = "Ounce";
-            //    unit = VolumeMeasurementUnit.Ounce;
-            //    conversionFactor = 2 * 3 * 24;
-            //}
             else if (fracTSP >= 24 * 3 || fracTSP == 18 || fracTSP == 36 || fracTSP == 54)
             {
-                unitString = "Tablespoon";
                 unit = VolumeMeasurementUnit.Tablespoon;
                 conversionFactor = 3 * 24;
             }
-            else if (fracTSP < 6)
-            {
-                //Only used if we end up with < 1/4 tsp due to serving size conversion!!!!
-                unitString = "A Dash/Pinch (less than 1/4 TSP) ";
-                var dashMeasure = unitString + "(" + ozConversion + " oz.)";
-                return dashMeasure;
-            }
             else //Must be Teaspoon or less
             {
-                unitString = "Teaspoon";
                 unit = VolumeMeasurementUnit.Teaspoon;
                 conversionFactor = 24;
 
             }
             //Get whats left from remainder
-            string measurement = "";
+            ingredient.VolumeMeasurementUnit = unit;
             int remainder = fracTSP % conversionFactor;
-            int howMany;
             if (remainder > 0)
             {
-                //Add s to unit
-                unitString += "s";
                 //round down to whole unit
                 fracTSP -= remainder;
                 //if fraction > .75, just round to next whole number.
                 if (remainder / conversionFactor > 0.75)
                 {
                     fracTSP += conversionFactor;
-                    howMany = fracTSP / conversionFactor;
-                    measurement = howMany + " " + unitString + "(" + ozConversion + " oz.)";
+                    ingredient.QuantityNumber = fracTSP / conversionFactor;
                 }
                 else
                 {
                     //Round up to fraction
-                    string fraction = RemainderToFraction(remainder, unit);
-                    howMany = fracTSP / conversionFactor;
-                    if (howMany == 0)
-                    {
-                        measurement = fraction + " " + unitString + "(" + ozConversion + " oz.)";
-
-                    }
-                    else
-                    {
-
-                        measurement = howMany + " " + fraction + " " + unitString + "(" + ozConversion + " oz.)";
-                    }
+                    ingredient.Fraction = DoubleToFraction(remainder/(double)conversionFactor);
+                    ingredient.QuantityNumber = fracTSP / conversionFactor;
                 }
 
             }
             else
             {
-                howMany = fracTSP / conversionFactor;
-                //add S to plurals
-                if (howMany != 1)
-                {
-                    unitString += "s";
-                }
-                measurement = howMany + " " + unitString + "(" + ozConversion + " oz.)";
+                ingredient.QuantityNumber = fracTSP / conversionFactor;
 
             }
-            return measurement;
+            return ingredient;
         }
 
         private ShoppingIngredient ApplyMassMeasurement(ShoppingIngredient ingredient)
