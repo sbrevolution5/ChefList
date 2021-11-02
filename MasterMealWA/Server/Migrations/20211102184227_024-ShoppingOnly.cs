70685c4c1d7a2839af4fd83de63a3ffb847772a0
sbrevolution5 <sbrevolution5@aim.com>
2021-11-02 14:42:57 -0400

    add property and migrations

---
+++ MasterMealWA/Server/Migrations/20211102184227_024-ShoppingOnly.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MasterMealWA.Server.Migrations
+{
+    public partial class _024ShoppingOnly : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShoppingOnly",
+                table: "IngredientType",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShoppingOnly",
+                table: "IngredientType");
+        }
+    }
+}
