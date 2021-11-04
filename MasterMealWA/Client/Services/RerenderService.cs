46ff8454b6941cda2f4ae99b87c72bf190c52507
sbrevolution5 <sbrevolution5@aim.com>
2021-11-04 12:23:41 -0400

    Revert "Revert "rerender service""
    This reverts commit 6d4cccfd4965b59bdedf0c950bed5faeec593d10.

---
+++ MasterMealWA/Client/Services/RerenderService.cs
@@ -0,0 +1,17 @@
+﻿using MasterMealWA.Client.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterMealWA.Client.Services
+{
+    public class RerenderService : IRerenderService
+    {
+        public event Action RefreshRequested;
+        public void CallRequestRefresh()
+        {
+            RefreshRequested?.Invoke();
+        }
+    }
+}
